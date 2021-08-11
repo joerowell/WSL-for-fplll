b5f90c06ac88efd8b463f01c00901a24ca28e9c5
Joe Rowell <Joe.rowell@rhul.ac.uk>
2021-08-11 15:46:17 +0100

    Fix merge

--- install_g6k.sh
+++ install_g6k.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 ## Step #1: Check that we have git installed
 if ! command -v git &> /dev/null
 then
@@ -8,7 +7,6 @@ then
     exit
 fi
 
-<<<<<<< HEAD
 ## If you don't update here installing GCC can fail
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -25,29 +23,10 @@ alias python=python3
 ## Some people won't have this, and that's OK, but make sure you actively read this before
 ## you run it!
 rm -rf ./mambaforge
-=======
-## Warning: this will nuke your existing conda installation.
-## Some people won't have this, and that's OK, but make sure you actively read this before
-## you run it!
-rm -rf ./anaconda3
-
-## If we have git installed then we also need to wget conda
-wget https://repo.continuum.io/archive/Anaconda3-5.3.1-Linux-x86_64.sh
-chmod +x Anaconda3-5.3.1-Linux-x86_64.sh
-./Anaconda3-5.3.1-Linux-x86_64.sh -b -p
-
-## Before we do anything else we need to make sure we've activated conda without logging
-## out. To do that, we make sure we know the current user
-username=$(whoami)
-## We need to escape the username here or this absolutely will not work. 
-eval -e "$(/Users/\$$username/anaconda/bin/conda shell.bash hook)"
-conda init
->>>>>>> 0b7330f... script
 
 ## Now we have a few different things we can do
 ## The best way to start is to install mamba, because it makes everything much faster
 curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh
-<<<<<<< HEAD
 sh Mambaforge-$(uname)-$(uname -m).sh -b -p
 source ~/mambaforge/bin/activate
 mamba init
@@ -62,19 +41,6 @@ mamba install -y libtool autoconf automake gmp mpfr
 mamba create -y -n g6k python=3.7
 mamba activate g6k
 mamba install -y sage
-=======
-sh Mambaforge-$(uname)-$(uname -m).sh 
-
-## Now we've installed mamba we can finally get started on the things we need
-mamba install libtool
-mamba install autoconf
-mamba install make
-
-## And now we can follow the rest of the default instructions
-mamba create -n g6k python=3.7
-mamba activate g6k
-mamba install -c mamba-forge sage
->>>>>>> 0b7330f... script
 
 git clone https://github.com/fplll/fplll
 cd fplll
@@ -103,7 +69,4 @@ python setup.py -q install
 cd ..
 
 echo "All finished!"
-<<<<<<< HEAD
 echo "Don't forget to run conda activate g6k when you log back in."
-=======
->>>>>>> 0b7330f... script
